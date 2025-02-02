95062415f8d539f20bf480b30348783ed4bb00f0
charyee <charyee@umich.edu>
2025-02-02 10:12:18 -0500

    users updates

--- src/Users.js
+++ src/Users.js
@@ -1,11 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { getFirestore, collection, getDocs } from 'firebase/firestore';
 import './Users.css';
+import './App.css';
 
 const db = getFirestore();
 
 function Users() {
   const [users, setUsers] = useState([]);
+  const [clickedCards, setClickedCards] = useState({});
+
+  const handleButtonClick = (index) => {
+    setClickedCards((prevState) => ({
+      ...prevState,
+      [index]: true,
+    }));
+  };
   
   useEffect(() => {
     const fetchUsers = async () => {
@@ -40,18 +49,25 @@ function Users() {
       ) : (
         users.map((user, index) => (
           <div key={index} className="user-card">
-            <div className="user-info">
-              <h3>{user.firstName} {user.lastName}</h3>
-              <p><strong>Year:</strong> {user.year}</p>
-              <p><strong>Major:</strong> {user.major}</p>
-              <p><strong>Availability:</strong></p>
-              <ul>
-                {Object.entries(getOrderedDays(user.availability)).map(([day, times], i) => (
-                  <li key={i}><strong>{day.charAt(0).toUpperCase() + day.slice(1)}:</strong> {Array.isArray(times) ? times.join(', ') : times}</li>
-                ))}
-              </ul>
-              <p><strong>Courses:</strong> {user.courses.join(', ')}</p>
-            </div>
+          {clickedCards[index] ? (
+              <div className="congrats-message">Congrats! Reach out to {user.firstName} at: @{user.email}</div>
+            ) : (
+              <div className="user-info">
+                <h3>{user.firstName} {user.lastName}</h3>
+                <p><strong>Year:</strong> {user.year}</p>
+                <p><strong>Major:</strong> {user.major}</p>
+                <p><strong>Availability:</strong></p>
+                <ul>
+                  {Object.entries(getOrderedDays(user.availability)).map(([day, times], i) => (
+                    <li key={i}><strong>{day.charAt(0).toUpperCase() + day.slice(1)}:</strong> {Array.isArray(times) ? times.join(', ') : times}</li>
+                  ))}
+                </ul>
+                <p className="card-courses"><strong>Courses:</strong> {user.courses.join(', ')}</p>
+                <div className="button-container">
+            <button className="match-button" onClick={() => handleButtonClick(index)}>Match!</button>
+          </div>
+              </div>
+            )}
           </div>
         ))
       )}
